de619d430303314b93081b8e8efdd5d2cbe3b51b
Juan Ramos <juancagb.17@gmail.com>
2019-02-13 12:46:22 -0500

    PHP example

--- index.php
+++ index.php
@@ -1,23 +1,78 @@
 <?php
-	if (isset($_GET['direccion']))
-	{
-		$direccion = $_GET['direccion'];
-		echo "Se ha ingresado 1 dirección: " . $direccion;
 
-		$google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" . urlencode($direccion) ."&key=AIzaSyApj6gLFFjVQ2sFXJ6k-U1yyiCfvBchybI";
-		$google_maps_json = file_get_contents($google_maps_url);
-		$google_maps_array = json_decode($google_maps_json, true);
-		$lat = $google_maps_array["results"][0]["geometry"]["location"]["lat"];
-		$lng = $google_maps_array["results"][0]["geometry"]["location"]["lng"];
-		echo $lat . "  " . $lng;
+$apiKey = 'REPLACE_WITH_YOUR_OWN_G_MAPS_GEOCODING_KEY';
 
-		$wikimedia_url = "https://commons.wikimedia.org/w/api.php?format=json&action=query&generator=geosearch&ggsprimary=all&ggsnamespace=6&ggsradius=500&ggscoord=51.5|11.95&ggslimit=1&prop=imageinfo&iilimit=1&iiprop=url&iiurlwidth=200&iiurlheight=200";
-		$wikimedia_json = file_get_contents($wikimedia_url);
-		$wikimedia_array = json_decode($wikimedia_json, true);
-		var_dump($wikimedia_array["query"]["pages"]["28971703"]["imageinfo"]);
+$getLatAndLongFromAddress = function ($address) use ($apiKey)
+{
+	$google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" . urlencode($address) ."&key=$apiKey";
+	$google_maps_json = file_get_contents($google_maps_url);
+	$google_maps_array = json_decode($google_maps_json, true);
+	// var_dump($google_maps_array);
+	$results = $google_maps_array["results"];
+	
+	if (sizeof($results) > 0) {
+		$location = $google_maps_array["results"][0]["geometry"]["location"];
+		$lat = $location["lat"];
+		$lng = $location["lng"];
+		return compact('lat', 'lng');
+	} else {
+		return null;
 	}
-?>
+	
+	/*
+	$arr = [];
+	$arr['lat'] = $lat;
+	$arr['lng'] = $lng;
+	return $arr;
+	*/
+};
 
+function showImageFromPage($page)
+{
+	echo '<p>' . $page['title'] . '</p>';
+	$image = $page['imageinfo'][0];
+
+	$width = $image['thumbwidth'];
+	$height = $image['thumbheight'];
+	$thumbUrl = $image['thumburl'];
+	$url = $image['url'];
+
+	echo "<a href='$url' target='_blank'>";
+	echo "<img src='$thumbUrl' width='$width' height = '$height'>";
+	echo "</a>";
+}
+
+function getImagesFromCoords($coords)
+{
+	$ggscoord = $coords['lat'] .'|' . $coords['lng']; // 51.5|11.95;
+	$wikimediaUrl = "https://commons.wikimedia.org/w/api.php?format=json&action=query&generator=geosearch&ggsprimary=all&ggsnamespace=6&ggsradius=500&ggscoord=$ggscoord&ggslimit=1&prop=imageinfo&iilimit=1&iiprop=url&iiurlwidth=200&iiurlheight=200";
+	$wikimediaJson = file_get_contents($wikimediaUrl);
+	$wikimediaArr = json_decode($wikimediaJson, true);
+
+	echo "<p>URL consultada: $wikimediaUrl</p>";
+
+	if (array_key_exists('query', $wikimediaArr)) {
+		$pages = $wikimediaArr['query']['pages'];
+		foreach ($pages as $page) {
+			showImageFromPage($page);
+		}
+	} else {
+		echo '<p>No se encontraron imágenes en la API de WikiMedia.</p>';
+	}
+}
+
+if (isset($_GET['direccion']))
+{
+	$direccion = $_GET['direccion'];
+	echo "Se ha ingresado esta dirección: " . $direccion;
+	$coords = $getLatAndLongFromAddress($direccion);
+	
+	if ($coords)
+		getImagesFromCoords($coords);
+	else
+		echo "No se encontraron resultados (en Google Maps Geocoding).";
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -31,5 +86,9 @@
 
 		<button type="submit">Consultar</button>
 	</form>
+
+	<?php foreach ($pages as $key => $page): ?>
+		<img src="" width="" height="">
+	<?php endforeach; ?>
 </body>
 </html>
